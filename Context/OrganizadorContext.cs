e94db2177c41f342dd15ec0cac95da53ad81c890
Breno Carneiro <breno.carneiro@gmail.com>
2023-01-11 18:15:41 -0300

    first commit

---
+++ Context/OrganizadorContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using GerenciadorDeTarefas.Models;
+
+namespace GerenciadorDeTarefas.Context
+{
+    public class OrganizadorContext : DbContext
+    {
+        public OrganizadorContext(DbContextOptions<OrganizadorContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Tarefa> Tarefas { get; set; }
+    }
+}
---
+++ Controllers/TarefaController.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using GerenciadorDeTarefas.Models;
+using GerenciadorDeTarefas.Context;
+
+namespace GerenciadorDeTarefas.Controllers
+{
+    public class TarefaController : Controller
+    {
+        
+        private readonly OrganizadorContext _context;
+
+        public TarefaController(OrganizadorContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var tarefas = _context.Tarefas.ToList();
+            return View(tarefas);
+        }
+        public IActionResult Criar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Criar(Tarefa tarefas)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Tarefas.Add(tarefas);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(tarefas);
+        }
+
+        public IActionResult Editar(int id)
+        {
+            var tarefa = _context.Tarefas.Find(id);
+            if (tarefa == null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                return View(tarefa);
+            }
+        }
+        [HttpPost]
+        public IActionResult Editar(Tarefa tarefas)
+        {
+            var tarefaDoBanco = _context.Tarefas.Find(tarefas.Id);
+
+            tarefaDoBanco.Titulo = tarefas.Titulo;
+            tarefaDoBanco.Descricao = tarefas.Descricao;
+            tarefaDoBanco.Data = tarefas.Data;
+            tarefaDoBanco.Status = tarefas.Status;
+
+            _context.Tarefas.Update(tarefaDoBanco);
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Detalhes(int id)
+        {
+            var tarefa = _context.Tarefas.Find(id);
+            if (tarefa == null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                return View(tarefa);
+            }
+        }
+
+        public IActionResult Deletar(int id)
+        {
+            var tarefa = _context.Tarefas.Find(id);
+            if (tarefa == null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                return View(tarefa);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Deletar(Tarefa tarefas)
+        {
+            var tarefaDoBanco = _context.Tarefas.Find(tarefas.Id);
+
+            _context.Tarefas.Remove(tarefaDoBanco);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+
+        }
+    }
+}
